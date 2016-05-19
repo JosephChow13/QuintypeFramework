39cc2714846c37e32c17c2f07af1d779badad8ee
Arjun P A <arjunpa@web.local>
2016-05-20 00:27:25 +0530

    Initial Commit

---
+++ QuintypeFramework/QuintypeFramework.h
@@ -0,0 +1,19 @@
+//
+//  QuintypeFramework.h
+//  QuintypeFramework
+//
+//  Created by Arjun P A on 20/05/16.
+//  Copyright © 2016 Arjun P A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuintypeFramework.
+FOUNDATION_EXPORT double QuintypeFrameworkVersionNumber;
+
+//! Project version string for QuintypeFramework.
+FOUNDATION_EXPORT const unsigned char QuintypeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuintypeFramework/PublicHeader.h>
+
+
